60710de424a636f1ea389a234e30cc13ed43a13e
Didik Setiawan <ds@didiksetiawan.com>
2020-06-09 23:18:41 +0700

    Failure test #3

--- testdir/test-1.sh
+++ testdir/test-1.sh
@@ -23,4 +23,4 @@ SUCCESS_CODE=$(($SUCCESS_CODE+1)) && echo success || echo failure
 [ -f ./recursed_a/two ] && [ ! -f ./two ] && [ ! -f ./twice_one ] && \
 SUCCESS_CODE=$(($SUCCESS_CODE+1)) && echo success || echo failure
 
-[ $SUCCESS_CODE -eq 4 ] && echo all test success || echo some test failure
+[ $SUCCESS_CODE -eq 4 ] && echo all test success || exit 1
