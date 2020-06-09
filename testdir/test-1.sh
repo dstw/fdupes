b811663c958bd01b0ef425144ee2af58ce2a41f5
Didik Setiawan <ds@didiksetiawan.com>
2020-06-09 23:10:54 +0700

    Failure test

--- testdir/test-1.sh
+++ testdir/test-1.sh
@@ -13,6 +13,7 @@ SUCCESS_CODE=$(($SUCCESS_CODE+1)) && echo success || echo failure
 SUCCESS_CODE=$(($SUCCESS_CODE+1)) && echo success || echo failure
 
 ./init.sh
+touch two
 ../fdupes -rNd -e recursed_a/ .
 [ -f ./recursed_a/two ] && [ ! -f ./two ] && [ ! -f ./twice_one ] && \
 SUCCESS_CODE=$(($SUCCESS_CODE+1)) && echo success || echo failure
