ed9fb929b534e6ddfe3bad188235131de389dff0
Didik Setiawan <ds@didiksetiawan.com>
2020-06-10 16:02:09 +0700

    Failure test #5

--- testdir/test-1.sh
+++ testdir/test-1.sh
@@ -16,6 +16,7 @@ echo \"$CMD_TEST\" success || echo \"$CMD_TEST\" fail
 
 ./init.sh
 CMD_TEST="../fdupes -rNd -e recursed_a/ ." && $CMD_TEST
+touch two
 [ -f ./recursed_a/two ] && [ ! -f ./two ] && [ ! -f ./twice_one ] && \
 SUCCESS_CODE=$(($SUCCESS_CODE+1)) && \
 echo \"$CMD_TEST\" success || echo \"$CMD_TEST\" fail
@@ -26,5 +27,5 @@ CMD_TEST="../fdupes -rNd --prefer recursed_a/ ." && $CMD_TEST
 SUCCESS_CODE=$(($SUCCESS_CODE+1)) && \
 echo \"$CMD_TEST\" success || echo \"$CMD_TEST\" fail
 
-[ $SUCCESS_CODE -eq 4 ] && echo all test success || \
-echo one or more test failed && exit 1
+[ $SUCCESS_CODE -ne 4 ] && echo one or more test failed && exit 1 || \
+echo all test success
