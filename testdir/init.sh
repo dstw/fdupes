6684a290d6fe7f9a714d236b1e23a9d0e49d1cfe
Didik Setiawan <ds@didiksetiawan.com>
2020-06-10 15:16:37 +0700

    Fix failure test #3

--- testdir/init.sh
+++ testdir/init.sh
@@ -1,20 +1,51 @@
 #!/bin/bash
 
 [ ! -d recursed_a ] && mkdir recursed_a
-[ ! -f recursed_a/five ] && echo "five" > recursed_a/five
-[ ! -f recursed_a/one ] && echo "one" > recursed_a/one
-[ ! -f recursed_a/two ] && echo "two" > recursed_a/two
+
+[ -f recursed_a/five ] && rm -f recursed_a/five && \
+echo "five" > recursed_a/five || echo "five" > recursed_a/five
+
+[ -f recursed_a/one ] && rm -f recursed_a/one && \
+echo "one" > recursed_a/one || echo "one" > recursed_a/one
+
+[ -f recursed_a/two ] && rm -f recursed_a/two && \
+echo "two" > recursed_a/two || echo "two" > recursed_a/two
+
 [ ! -d recursed_b ] && mkdir recursed_b
-[ ! -f recursed_b/four ] && echo "four" > recursed_b/four
-[ ! -f recursed_b/one ] &&  echo "one" > recursed_b/one
-[ ! -f recursed_b/three ] && echo "three" > recursed_b/three
-[ ! -f recursed_b/two_plus_one ] && echo "three" > recursed_b/two_plus_one
-[ ! -f twice_one ] && echo "two" > twice_one
-[ ! -f two ] && echo "two" > two
-[ ! -f with\ spaces\ a ] && echo "with spaces test" > with\ spaces\ a
-[ ! -f with\ spaces\ b ] && echo "with spaces" > with\ spaces\ b
-touch zero_a
-touch zero_b
-[ ! -h symlink_dir ] && ln -s recursed_a symlink_dir
-[ ! -h symlink_two ] && ln -s two symlink_two
-[ ! -f nine_upsidedown ] && echo "six" > nine_upsidedown
+
+[ -f recursed_b/four ] && rm -f recursed_b/four && \
+echo "four" > recursed_b/four || echo "four" > recursed_b/four
+
+[ -f recursed_b/one ] && rm -f recursed_b/one && \
+echo "one" > recursed_b/one || echo "one" > recursed_b/one
+
+[ -f recursed_b/three ] && rm -f recursed_b/three && \
+echo "three" > recursed_b/three || echo "three" > recursed_b/three
+
+[ -f recursed_b/two_plus_one ] && rm -f recursed_b/two_plus_one && \
+echo "three" > recursed_b/two_plus_one || echo "three" > recursed_b/two_plus_one
+
+[ -f twice_one ] && rm -f twice_one && \
+echo "two" > twice_one || echo "two" > twice_one
+
+[ -f two ] && rm -f two && \
+echo "two" > two || echo "two" > two
+
+[ -f with\ spaces\ a ] && rm -f with\ spaces\ a && \
+echo "with spaces" > with\ spaces\ a || echo "with spaces" > with\ spaces\ a
+
+[ -f with\ spaces\ b ] && rm -f with\ spaces\ b && \
+echo "with spaces" > with\ spaces\ b || echo "with spaces" > with\ spaces\ b
+
+[ -f zero_a ] && rm -f zero_a && touch zero_a || touch zero_a
+
+[ -f zero_b ] && rm -f zero_b && touch zero_b || touch zero_b
+
+[ -h symlink_dir ] && rm -f symlink_dir && \
+ln -s recursed_a symlink_dir || ln -s recursed_a symlink_dir
+
+[ -h symlink_two ] && rm -f symlink_two && \
+ln -s two symlink_two || ln -s two symlink_two
+
+[ -f nine_upsidedown ] && rm -f nine_upsidedown && \
+echo "six" > nine_upsidedown || echo "six" > nine_upsidedown
--- testdir/test-1.sh
+++ testdir/test-1.sh
@@ -3,24 +3,29 @@
 SUCCESS_CODE=0
 
 ./init.sh
-../fdupes -rNd -e twice_one .
+CMD_TEST="../fdupes -rNd -e twice_one ." && $CMD_TEST
 [ ! -f ./recursed_a/two ] && [ ! -f ./two ] && [ -f ./twice_one ] && \
-SUCCESS_CODE=$(($SUCCESS_CODE+1)) && echo success || echo failure
+SUCCESS_CODE=$(($SUCCESS_CODE+1)) && \
+echo \"$CMD_TEST\" success || echo \"$CMD_TEST\" fail
 
 ./init.sh
-../fdupes -rNd --prefer twice_one .
+CMD_TEST="../fdupes -rNd --prefer twice_one ." && $CMD_TEST
 [ ! -f ./recursed_a/two ] && [ ! -f ./two ] && [ -f ./twice_one ] && \
-SUCCESS_CODE=$(($SUCCESS_CODE+1)) && echo success || echo failure
+SUCCESS_CODE=$(($SUCCESS_CODE+1)) && \
+echo \"$CMD_TEST\" success || echo \"$CMD_TEST\" fail
 
 ./init.sh
-../fdupes -rNd -e recursed_a/ .
+CMD_TEST="../fdupes -rNd -e recursed_a/ ." && $CMD_TEST
 touch two
 [ -f ./recursed_a/two ] && [ ! -f ./two ] && [ ! -f ./twice_one ] && \
-SUCCESS_CODE=$(($SUCCESS_CODE+1)) && echo success || echo failure
+SUCCESS_CODE=$(($SUCCESS_CODE+1)) && \
+echo \"$CMD_TEST\" success || echo \"$CMD_TEST\" fail
 
 ./init.sh
-../fdupes -rNd --prefer recursed_a/ .
+CMD_TEST="../fdupes -rNd --prefer recursed_a/ ." && $CMD_TEST
 [ -f ./recursed_a/two ] && [ ! -f ./two ] && [ ! -f ./twice_one ] && \
-SUCCESS_CODE=$(($SUCCESS_CODE+1)) && echo success || echo failure
+SUCCESS_CODE=$(($SUCCESS_CODE+1)) && \
+echo \"$CMD_TEST\" success || echo \"$CMD_TEST\" fail
 
-[ $SUCCESS_CODE -eq 4 ] && echo all test success || exit 1
+[ $SUCCESS_CODE -eq 4 ] && echo all test succeeded || \
+echo one or more test failed && exit 1
